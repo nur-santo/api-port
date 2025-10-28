a941fd0dc1da78ade6804f03ea4e9fab30e30896
nur <nursantor16@gmail.com>
2025-10-28 14:35:57 +0700

    Update index.ts

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import "jsr:@supabase/functions-js/edge-runtime.d.ts";
 import { createClient } from "jsr:@supabase/supabase-js@2";
 
 const supabase = createClient(Deno.env.get("SUPABASE_URL"), Deno.env.get("SUPABASE_SERVICE_ROLE_KEY"));
+// ENDPOINT
 const API_MAP = {
   // PICSART
   removebg: "https://api.picsart.io/tools/1.0/removebg",
